02360d40a3e626df8777025ce3b64ded051c8111
dahliasan <laninaesloco@gmail.com>
2022-12-26 08:57:52 +1100

    added gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import './styles.css'
 import { Nunito, Varela_Round } from '@next/font/google'
-import Script from 'next/script'
+import TagManager from 'react-gtm-module'
 
 const nunito = Nunito({ subsets: ['latin'] })
 const varelaRound = Varela_Round({
@@ -10,27 +10,13 @@ const varelaRound = Varela_Round({
 })
 
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-52Q8NF2' })
+  }, [])
+  return <Component {...pageProps} />
+
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-HXP7C8JEC8"
-      />
-          
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-HXP7C8JEC8', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-      />
       <style jsx global>{`
         :root {
           --body-font: ${nunito.style.fontFamily};
