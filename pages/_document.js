f0bc9041558e8088b2017c62a23de9955d249ef5
dahliasan <laninaesloco@gmail.com>
2022-12-26 08:17:24 +1100

    fixed meta data

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import Image from 'next/image'
+
 export default function Document() {
   return (
     <Html>
       <Head>
         <meta
           property="og:title"
-          content="Your Personal AI Statistician"
+          content="tldr; 1 min summary generator"
           key="title"
         />
         <meta
@@ -14,10 +16,7 @@ export default function Document() {
           content="build with buildspace"
           key="description"
         />
-        <meta
-          property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
-        />
+        <meta property="og:image" content="https://iili.io/HI8zG7s.png" />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
