c3710d75858455a3a42520d92c02b279f808ed22
dahliasan <laninaesloco@gmail.com>
2022-12-26 08:46:20 +1100

    added ga4

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import './styles.css'
 import { Nunito, Varela_Round } from '@next/font/google'
+import Script from 'next/script'
 
 const nunito = Nunito({ subsets: ['latin'] })
 const varelaRound = Varela_Round({
@@ -11,6 +12,25 @@ const varelaRound = Varela_Round({
 function App({ Component, pageProps }) {
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-HXP7C8JEC8"
+      />
+          
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HXP7C8JEC8', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <style jsx global>{`
         :root {
           --body-font: ${nunito.style.fontFamily};
